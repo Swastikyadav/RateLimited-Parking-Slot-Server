56f02fd45b8ee23903467489cf84a3b616f3de6f
Swastikyadav <swastikyadav.dev@gmail.com>
2021-05-02 20:55:47 +0530

    implemented rate limiter with redis

--- server.js
+++ server.js
@@ -11,6 +11,43 @@ const apiRouter = require("./routes/v1/api");
 
 const PORT = process.env.PORT || 3000;
 
+const redis = require("ioredis");
+const client = redis.createClient({
+  port: 6379,
+  host: 'localhost',
+});
+
+client.on('connect', function() {
+  console.log("connected");
+});
+
+app.use(async (req, res, next) => {
+  async function isOverLimit(ip) {
+    let res;
+    try {
+      res = await client.incr(ip);
+    } catch(err) {
+      throw err;
+    }
+
+    console.log(`${ip} has value: ${res}`);
+
+    if (res > 10) {
+      return true
+    }
+    client.expire(ip, 10)
+  }
+
+  let overLimit = await isOverLimit(req.headers['x-forwarded-for'] || req.connection.remoteAddress)
+
+  if (overLimit) {
+    res.status(429).json('Too many requests - try again later')
+    return
+  }
+
+  next();
+})
+
 app.use(customBodyParser);
 
 app.use("/", indexRouter);
